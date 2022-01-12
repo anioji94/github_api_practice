467533afb81bb19cfca3004a959f39b494670aa7
Aniruddh Ojha <ojhaa94@gmail.com>
2022-01-12 21:31:19 +0000

    removes unnecessary function

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 let divOutput = document.getElementById('test')
 let name = document.getElementById('name')
 
-fetch('https://api.github.com/users/anioji94')
-		.then(response => response.json()) //Converting the response to a JSON object
-		.then( data => document.body.append())
-		.catch( error => console.error(error));
-
 console.log(divOutput)
 let howdy = () => {
 	if(!name.value){
