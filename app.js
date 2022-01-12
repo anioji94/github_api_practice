d52395ecf059733c9d8574e06fe02753dfa15248
Aniruddh Ojha <ojhaa94@gmail.com>
2022-01-12 22:42:03 +0000

    adds user lookup on github

--- app.js
+++ app.js
@@ -1,19 +1,48 @@
 let divOutput = document.getElementById('test')
-let name = document.getElementById('name')
+let usernameField = document.getElementById('username')
 
-console.log(divOutput)
-let howdy = () => {
-	if(!name.value){
-		input = 'Ani'
+function usernameCheck() {
+	if(!usernameField.value) {
+		username = 'defunkt'
 	} else {
-		input = name.value
+		username = usernameField.value
 	}
-	divOutput.innerHTML += `Hello, I am ${input}<br/>`;
-	name.value = '';
+}
+
+// let username = usernameField.value
+
+function usernameClear() {
+	username = ""
+}
+
+let howdy = () => {
+	usernameCheck()
+	divOutput.innerHTML += `Hello, I am ${username}<br/>`;
+	usernameClear();
 }
 
 let clearDiv = () => {
 	divOutput.innerText = '';
 	console.log('clear')
+}
+
+let file = "https://api.github.com/zen"
+
+function test() {
+	fetch(file)
+	.then(x => x.text())
+	.then(y => console.log(y))
+}
+
+let userURL = "https://api.github.com/users/"
+
+function getUserProfile() {
+	usernameCheck();
+
+	fetch(userURL + username)
+		.then(userURL => userURL.text())
+		.then(userInfo => console.log(userInfo))
+	
+	usernameClear()
 }
 
