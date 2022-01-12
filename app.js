11694429b28241af0b4658a472fd9996e57e81a7
Aniruddh Ojha <ojhaa94@gmail.com>
2022-01-12 21:27:09 +0000

    text input interacts with button

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let divOutput = document.getElementById('test')
+let name = document.getElementById('name')
 
 fetch('https://api.github.com/users/anioji94')
 		.then(response => response.json()) //Converting the response to a JSON object
@@ -6,8 +7,14 @@ fetch('https://api.github.com/users/anioji94')
 		.catch( error => console.error(error));
 
 console.log(divOutput)
-let howdy = (input = "Ani") => {
+let howdy = () => {
+	if(!name.value){
+		input = 'Ani'
+	} else {
+		input = name.value
+	}
 	divOutput.innerHTML += `Hello, I am ${input}<br/>`;
+	name.value = '';
 }
 
 let clearDiv = () => {
