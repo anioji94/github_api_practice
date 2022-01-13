11b1f6e315a7225cece8bb73e740e03345a39b47
Aniruddh Ojha <ojhaa94@gmail.com>
2022-01-13 10:51:58 +0000

    use await instead of then

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let divOutput = document.getElementById('test')
 let usernameField = document.getElementById('username')
 
+// Housekeeping Functions
+
 function usernameCheck() {
 	if(!usernameField.value) {
 		username = 'defunkt'
@@ -9,22 +11,29 @@ function usernameCheck() {
 	}
 }
 
-// let username = usernameField.value
-
 function usernameClear() {
 	username = ""
 }
 
-let howdy = () => {
-	usernameCheck()
-	divOutput.innerHTML += `Hello, I am ${username}<br/>`;
-	usernameClear();
+function usernameFieldClear() {
+	usernameField.value = ''
 }
 
 let clearDiv = () => {
 	divOutput.innerText = '';
 	console.log('clear')
 }
+
+// Test Functions
+
+let howdy = () => {
+	usernameCheck()
+	divOutput.innerHTML += `Hello, I am ${username}<br/>`;
+	usernameClear();
+	usernameFieldClear();
+}
+
+// API Test Functions
 
 let file = "https://api.github.com/zen"
 
@@ -34,15 +43,22 @@ function test() {
 	.then(y => console.log(y))
 }
 
+// API Functions
+
 let userURL = "https://api.github.com/users/"
 
 function getUserProfile() {
 	usernameCheck();
 
-	fetch(userURL + username)
-		.then(userURL => userURL.text())
-		.then(userInfo => console.log(userInfo))
+	// fetch(userURL + username)
+	// 	.then(userURL => userURL.text())
+	// 	.then(userInfo => console.log(userInfo))
+
+	let userInfo = await fetch(userURL + username)
+	let userInfoText = await userInfo.text()
+	console.log(userInfoText)
 	
-	usernameClear()
+	usernameClear();
+	usernameFieldClear();
 }
 
